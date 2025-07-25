d803da1a1bf5928f6ea30ba3755169a8f585932f
Adam Wahana <60256064+adamwahanaf@users.noreply.github.com>
2025-07-25 19:13:53 +0700

    Perbaikan besar modul transaksi
    Perubahan utama yang dilakukan:
    - Penyimpanan Item Baru: Cara menyimpan daftar barang diubah dari daftar biasa menjadi 'dictionary'. Ini membuat pencarian, pembaruan, dan penghapusan item jadi jauh lebih cepat.
    - Pengecekan Input yang Kuat: Menambahkan sistem pengecekan data yang masuk (nama barang, jumlah, harga) secara menyeluruh. Ini mencegah kesalahan data dan membuat program lebih aman.
    - Penanganan Kesalahan: Program sekarang bisa mengatasi kesalahan input dengan lebih baik, memberikan pilihan kepada pengguna untuk mencoba lagi atau kembali.
    - Kode Lebih Rapi: Banyak bagian kode yang diulang-ulang sudah dirapikan menjadi fungsi-fungsi kecil yang bisa dipakai berkali-kali. Ini membuat kode lebih bersih dan mudah dipahami.
    - Perbaikan Fitur Inti: Semua fitur utama seperti menambah, mengubah nama, mengubah jumlah, mengubah harga, menghapus item, dan menghitung total belanja sudah diperbaiki dan bekerja dengan benar.

--- transaction.py
+++ transaction.py
@@ -1,151 +1,207 @@
 class Transaction:
     def __init__(self):
-        self.list_items = []
+        self.list_items = {}
     
     def add_item(self):
         print(("\n"+"-" * 15 + " Menu Buat Pesanan " + "-" * 15 +"\n"))
-        
-        self.item_name = input("Nama item: ")
-        self.item_name = self.item_name.title()
 
         while True:
+            item_name = input("Nama item: ")
+            is_item_name_empty = self._is_item_name_empty(item_name)
+            if not is_item_name_empty:
+                continue
+            normalize_item_name = self._normalize_name(item_name)
+            if normalize_item_name in self.list_items:
+                print(f"Peringatan: Item {item_name} sudah ada dalam daftar.")
+                print("\n1. Lanjut tambah item lain" \
+                "2. Kembali ke menu awal untuk update item")
+                is_choice_num_1 =  self._get_choice_1_2("Pilih menu")
+                if not is_choice_num_1:
+                    break
+        while True:
             try:
-                self.item_qty = int(input("Jumlah item: "))
-                break
+                item_qty = int(input("Jumlah item: "))
+                is_item_qty_less_than_1 = self._is_less_than_1(item_qty)
+                if not is_item_qty_less_than_1:
+                    break
+                else:
+                    print("Error: Tidak boleh angka 0 atau minus")
             except ValueError:
                 print('Error: Salah input, hanya input angka saja')
         while True:    
             try:
-                self.price_per_item = int(input("Harga per item: "))
-                break
+                price_per_item = int(input("Harga per item: "))
+                is_price_per_item_less_than_1 = self._is_less_than_1(price_per_item)
+                if not is_price_per_item_less_than_1:
+                    break
+                else:
+                    print("Error: Tidak boleh angka 0 atau minus")
             except ValueError:
                 print('Error: Salah input, hanya input angka saja')
 
-        self.list_items.append({
-            "Item Name" : self.item_name,
-            "Quantity": self.item_qty,
-            "Price per Item": self.price_per_item
-        })
+        self.list_items[normalize_item_name] = {
+            "Original Name": item_name,
+            "Quantity": item_qty,
+            "Price per Item": price_per_item
+        }
 
-        return self.display_order("Item yang dibeli adalah:")
+        self.display_order("Item yang dibeli adalah:")
+
+        return self._get_y_n_choice("Tambah item lagi?")
     
     def update_item_name(self):
         while True:
-            
-            self.old_item_name = input("Nama item lama: ")
-            self.old_item_name = self.old_item_name.title()
-            
-            for item in self.list_items:
-                if item["Item Name"] == self.old_item_name:
-                    self.new_item_name = input("Nama item baru: ")
-                    self.new_item_name = self.new_item_name.title()
-                    item["Item Name"] = self.new_item_name
-                    return self.display_order(f"Item yang dibeli adalah\n" 
-                                              f"(setelah merubah nama item {self.old_item_name} "
-                                              f"menjadi {self.new_item_name})")
-            
-            print("Error: Tidak ditemukan nama item yang dimasukkan")
+            old_item_name = input("Nama item lama: ")
+            if not old_item_name.strip():
+                print("Nama item tidak boleh kosong")
+                continue
+            normalize_old_item_name = self._normalize_name(old_item_name)
+            if normalize_old_item_name in self.list_items:
+                break
+            else:
+                print("Error: Tidak ditemukan nama item yang dimasukkan")
+        while True:
+            new_item_name = input("Nama item baru: ")
+            normalize_new_item_name = self._normalize_name(new_item_name)
+            if normalize_new_item_name in self.list_items and normalize_new_item_name != normalize_old_item_name:
+                print(f"Peringatan: Item {new_item_name} sudah ada dalam daftar.")
+                print("\n1. Lanjut update nama item" \
+                "2. Kembali ke menu sebelumnya")
+                is_choice_num_1 =  self._get_choice_1_2("Pilih menu")
+                if not is_choice_num_1:
+                    break
+            else:
+                break
+        tmp = self.list_items.pop(normalize_old_item_name)
+        self.list_items[normalize_new_item_name] = tmp
+        self.list_items[normalize_new_item_name]["Original Name"] = new_item_name
+        self.display_order(f"Item yang dibeli adalah\n" 
+                            f"(setelah merubah nama item {old_item_name} "
+                            f"menjadi {new_item_name})")
+        
+        return self._get_y_n_choice("Update item lagi?")
     
     def update_item_qty(self):
         while True:
-            
-            self.item_name = input("Nama item: ")
-            self.item_name = self.item_name.title()
-
-            self.match_name = any([item["Item Name"] == self.item_name for item in self.list_items])
-            if self.match_name:
+            item_name = input("Nama item: ")
+            if not item_name.strip():
+                print("Nama item tidak boleh kosong")
+                continue
+            normalize_item_name = self._normalize_name(item_name)
+            if normalize_item_name in self.list_items:
                 break
             else:
-                print("Error: Tidak ditemukan nama item yang dimasukkan")
-
+                print(f'Error: {item_name} tidak ditemukan')
+                print("\n1. Lanjut update nama item" \
+                "2. Kembali ke menu sebelumnya")
+                is_choice_num_1 =  self._get_choice_1_2("Pilih menu")
+                if not is_choice_num_1:
+                    break
         while True:
             try:
-                self.new_item_qty = int(input("Jumlah item baru: "))
-                if self.new_item_qty > 0:
-                    for item in self.list_items:
-                        if item["Item Name"] == self.item_name and item["Quantity"] != self.new_item_qty:
-                            item["Quantity"] = self.new_item_qty
-                            return self.display_order(f"Item yang dibeli adalah\n" 
-                                              f"(setelah merubah jumlah item {self.item_name} "
-                                              f"menjadi {self.new_item_qty} buah)")
+                new_item_qty = int(input("Jumlah item baru: "))
+                is_item_qty_less_than_1 = self._is_less_than_1(new_item_qty)
+                if not is_item_qty_less_than_1:
+                    break
                 else:
-                    print("Error: Jumlah item tidak boleh kurang dari 1")
+                    print("Error: Tidak boleh angka 0 atau minus")
             except ValueError:
                 print('Error: Salah input, hanya input angka saja')
+        if self.list_items[normalize_item_name]["Quantity"] != new_item_qty:
+            self.list_items[normalize_item_name]["Quantity"] = new_item_qty
+            self.display_order(f"Item yang dibeli adalah\n" 
+                               f"(setelah merubah jumlah item {item_name} "
+                               f"menjadi {new_item_qty})")
+            
+            return self._get_y_n_choice("Update item lagi?")
         
-    
     def update_price_per_item(self):
         while True:
-            
-            self.item_name = input("Nama item: ")
-            self.item_name = self.item_name.title()
-
-            self.match_name = any([item["Item Name"] == self.item_name for item in self.list_items])
-            if self.match_name:
+            item_name = input("Nama item: ")
+            if not item_name.strip():
+                print("Nama item tidak boleh kosong")
+                continue
+            normalize_item_name = self._normalize_name(item_name)
+            if normalize_item_name in self.list_items:
                 break
             else:
-                print("Error: Tidak ditemukan nama item yang dimasukkan")
-        
+                print(f'Error: {item_name} tidak ditemukan')
+                print("\n1. Lanjut update nama item" \
+                "2. Kembali ke menu sebelumnya")
+                is_choice_num_1 =  self._get_choice_1_2("Pilih menu")
+                if not is_choice_num_1:
+                    break
         while True:
             try:
-                self.new_price_per_item = int(input("Jumlah item baru: "))
-                if self.new_price_per_item > 0:
-                    for item in self.list_items:
-                        if item["Item Name"] == self.item_name and item["Price per Item"] != self.new_price_per_item:
-                            item["Price per Item"] = self.new_price_per_item
-                            return self.display_order(f"Item yang dibeli adalah\n" 
-                                              f"(setelah merubah harga per item {self.item_name} "
-                                              f"menjadi {self.new_price_per_item} buah)")
+                new_price_per_item = int(input("Jumlah item baru: "))
+                is_item_qty_less_than_1 = self._is_less_than_1(new_price_per_item)
+                if not is_item_qty_less_than_1:
+                    break
                 else:
-                    print("Error: harga per item tidak boleh 0")
+                    print("Error: Tidak boleh angka 0 atau minus")
             except ValueError:
                 print('Error: Salah input, hanya input angka saja')
+        
+        if self.list_items[normalize_item_name]["Price per Item"] != new_price_per_item:
+            self.list_items[normalize_item_name]["Price per Item"] = new_price_per_item
+            self.display_order(f"Item yang dibeli adalah\n"
+                               f"(setelah merubah harga per item {item_name} "
+                               f"menjadi Rp.{new_price_per_item}")
+            
+            return self._get_y_n_choice("Update item lagi?")
     
     def delete_item(self):
         while True:
-            
-            self.item_name = input("Nama item: ")
-            self.item_name = self.item_name.title()
-
-            self.match_name = any([item["Item Name"] == self.item_name for item in self.list_items])
-            if self.match_name:
+            item_name = input("Nama item: ")
+            if not item_name.strip():
+                print("Nama item tidak boleh kosong")
+                continue
+            normalize_item_name = self._normalize_name(item_name)
+            if normalize_item_name in self.list_items:
                 break
             else:
-                print("Error: Tidak ditemukan nama item yang dimasukkan")
+                print(f'Error: {item_name} tidak ditemukan')
+                print("\n1. Lanjut hapus item" \
+                "2. Kembali ke menu sebelumnya")
+                is_choice_num_1 =  self._get_choice_1_2("Pilih menu")
+                if not is_choice_num_1:
+                    break
 
-        self.new_list_items = [item for item in self.list_items if item["Item Name"] != self.item_name]
-
-        self.list_items = self.new_list_items
+        del self.list_items[normalize_item_name]
 
         if len(self.list_items) < 1:
-            return f"\nSemua item berhasil dihapus"
+            print(f"\nSemua item berhasil dihapus")
+            return False
         else:
-            return self.display_order(f"Item yang dibeli adalah\n"
-                                      f"(setelah menghapus item {self.item_name})")
+            self.display_order(f"Item yang dibeli adalah\n"
+                               f"(setelah menghapus item {self.item_name})")
+            
+            return self._get_y_n_choice("Hapus item lagi?")
     
     def reset_transaction(self):
         self.list_items.clear()
         print("Semua item berhasil di-delete!\n")
     
     def check_order(self):
-        for item in self.list_items:
+        for item_name in self.list_items:
 
-            if (not item.get('Item Name', '').strip() or
-                item.get('Quantity', 0) <= 0 or
-                item.get('Price per Item', 0) <= 0):
+            if (not self.list_items[item_name].get('Original Name', '').strip() or
+                self.list_items[item_name].get('Quantity', 0) <= 0 or
+                self.list_items[item_name].get('Price per Item', 0) <= 0):
                 return False
             
         return True
     
     def display_order(self, prompt):
-        self.prompt = prompt
-        print(f"\n{self.prompt}")
+        print(f"\n{prompt}")
         print("+"+"-"*4 + "+"+"-"*14 + "+"+"-"*13 + "+"+"-"*12 + "+")
         print("| No " + "| Nama Item ".ljust(15) + "| Jumlah Item " + "| Harga/Item |")
         print("+"+"-"*4 + "+"+"-"*14 + "+"+"-"*13 + "+"+"-"*12 + "+")
         
-        for num, item in enumerate(self.list_items):
-            print(f"| {num+1} ".ljust(5) + f"| {item['Item Name']} ".ljust(15) + f"| {item['Quantity']} ".ljust(14) + f"| {item['Price per Item']} ".ljust(13) + "|")
+        for num, item_name in enumerate(self.list_items):
+            print(f"| {num+1} ".ljust(5) + f"| {self.list_items[item_name]["Original Name"]} ".ljust(15) 
+                  + f"| {self.list_items[item_name]["Quantity"]} ".ljust(14) 
+                  + f"| {self.list_items[item_name]["Price per Item"]} ".ljust(13) + "|")
         
         print("+"+"-"*4 + "+"+"-"*14 + "+"+"-"*13 + "+"+"-"*12 + "+")
     
@@ -155,44 +211,78 @@ class Transaction:
         print("| No " + "| Nama Item ".ljust(15) + "| Jumlah Item " + "| Harga/Item " + "| Total Harga | ")
         print("+"+"-"*4 + "+"+"-"*14 + "+"+"-"*13 + "+"+"-"*12 + "+"+"-"*13+"+")
         
-        for num, item in enumerate(self.final_list_items):
-            print(f"| {num+1} ".ljust(5) + f"| {item['Item Name']} ".ljust(15) + f"| {item['Quantity']} ".ljust(14) + f"| {item['Price per Item']} ".ljust(13) + f"| {item['Total Price']}".ljust(13) + " |")
+        for num, item_name in enumerate(self.final_list_items):
+            print(f"| {num+1} ".ljust(5) + f"| {self.final_list_items[item_name]["Original Name"]} ".ljust(15) 
+                  + f"| {self.final_list_items[item_name]["Quantity"]} ".ljust(14) 
+                  + f"| {self.final_list_items[item_name]["Price per Item"]} ".ljust(13) 
+                  + f"| {self.final_list_items[item_name]["Total Price"]}".ljust(13) + " |")
         
         print("+"+"-"*4 + "+"+"-"*14 + "+"+"-"*13 + "+"+"-"*12 + "+"+"-"*13+"+")
 
     def total_price_per_item(self):
         self.final_list_items = self.list_items.copy()
-        for item in self.final_list_items:
-            item['Total Price'] = item['Quantity']*item['Price per Item']
+        for item_name in self.final_list_items:
+            self.final_list_items[item_name]['Total Price'] = (
+                self.final_list_items[item_name]['Quantity'] *
+                self.final_list_items[item_name]['Price per Item']
+                )
         return self.final_list_items
     
     def total_price(self):
-        self.total_price_every_item = [item["Total Price"] for item in self.final_list_items if item['Total Price'] !=0]
-        self.total_price_all_item = sum(self.total_price_every_item)
-        print("+"+"-"*4 + "+"+"-"*14 + "+"+"-"*13 + "+"+"-"*12 + "+"+"-"*13+"+")
-        print("| No " + "| Nama Item ".ljust(15) + "| Jumlah Item " + "| Harga/Item " + "| Total Harga | ")
-        print("+"+"-"*4 + "+"+"-"*14 + "+"+"-"*13 + "+"+"-"*12 + "+"+"-"*13+"+")
-        
-        for num, item in enumerate(self.final_list_items):
-            print(f"| {num+1} ".ljust(5) + f"| {item['Item Name']} ".ljust(15) + f"| {item['Quantity']} ".ljust(14) + f"| {item['Price per Item']} ".ljust(13) + f"| {item['Total Price']}".ljust(13) + " |")
-        
-        print("+"+"-"*4 + "+"+"-"*14 + "+"+"-"*13 + "+"+"-"*12 + "+"+"-"*13+"+")
-        print(f'\nTotal Belanja yang harus dibayarkan adalah Rp.{self.total_price_all_item}')
+        total_price_every_item = [self.final_list_items[item_name]['Total Price'] 
+                                  for item_name in self.final_list_items]
+        total_price_all_item = sum(total_price_every_item)
+        diskon = 0
+        if total_price_all_item > 500000:
+            diskon = 0.1
+        elif total_price_all_item > 300000:
+            diskon = 0.08
+        elif total_price_all_item > 200000:
+            diskon = 0.05
+        total_price_all_item_after_discount = total_price_all_item - (total_price_all_item*diskon)
+        # Improved clarity:
+        if diskon > 0:
+            print(f'Anda mendapatkan diskon sebanyak {diskon*100:.0f}% '
+                  f'karena telah berbelanja senilai Rp.{total_price_all_item:,.0f} (sebelum diskon).')
+            print(f'\nTotal Belanja yang harus dibayarkan adalah Rp.{total_price_all_item_after_discount:,.0f}')
+        else:
+            print(f'\nTotal Belanja yang harus dibayarkan adalah Rp.{total_price_all_item_after_discount:,.0f}')
     
-    def transaction_again(self, prompt):
-        self.prompt = prompt
+    def _get_y_n_choice(self, prompt):
         while True:
-            self.again = input(f"\n{self.prompt} (y/n): ")
-            self.again = self.again.lower()
+            again = input(f"\n{prompt} (y/n): ")
+            again = again.lower()
             
-            if self.again == 'n':
+            if again == 'n':
                 return False
-            elif self.again == 'y':
+            elif again == 'y':
                 return True
             else:
                 print('Error: Salah input, hanya input sesuai pilihan (y/n)')
     
+    def _normalize_name(self, name):
+        """Normalizes an item name for consistent comparison."""
+        return name.strip().replace(" ", "").lower()
     
-
-
-
+    def _get_choice_1_2(self, prompt):
+        while True:
+            try:
+                user_input = int(input(f"\n{prompt} (1/2): "))
+                if user_input == 1:
+                    return True
+                elif user_input == 2:
+                    return False
+                else:
+                    print('Error: Salah input, hanya input sesuai pilihan (1/2)')
+            except ValueError:
+                print('Error: Salah input, hanya input sesuai pilihan (1/2)')
+    
+    def _is_item_name_empty(self, item_name):
+        if not item_name.strip():
+            return True # Ini berarti "ya, nama item ini kosong/hanya spasi"
+        return False # Ini berarti "tidak, nama item ini tidak kosong"
+    
+    def _is_less_than_1(self, data):
+        if data < 1:
+            return True
+        return False
